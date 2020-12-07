167a4e5bbdedb34f247c9a9e25a73b720305d506
reddns4 <reddns4@yandex.ru>
2020-12-07 18:10:06 +0400

    [*] 7-8 Complete

--- py-test/7-8.py
+++ py-test/7-8.py
@@ -1,2 +1,16 @@
 # Условие
 # Дан список, упорядоченный по неубыванию элементов в нем. Определите, сколько в нем различных элементов.
+
+a = [int(i) for i in input().split()]
+
+count = 0
+
+for i in range(0, len(a)):
+    if a[i-1] != a[i]:
+        count += 1
+        
+if count == 0:
+    count += 1
+
+
+print(count)
