fb2f9616dec2879a33cb95f928922066ee73c86d
reddns4 <reddns4@yandex.ru>
2020-12-08 12:50:27 +0400

    [*] 7-10 Complete

---
+++ py-test/7-10.py
@@ -0,0 +1,22 @@
+# Условие
+# В списке все элементы различны. Поменяйте местами минимальный и максимальный элемент этого списка.
+
+a = [int(i) for i in input().split()]
+
+max_val = a[0] 
+min_val = a[0] 
+max_index = 0
+min_index = 0
+
+for i in range(0, len(a)):
+    if max_val <= a[i]:
+        max_val = a[i]
+        max_index = i
+    
+    if min_val >= a[i]:
+        min_val = a[i]
+        min_index = i 
+        
+a[max_index], a[min_index] = a[min_index], a[max_index]
+
+print(' '.join([str(i) for i in a]))
